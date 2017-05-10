80339f9c53f9af4a6e7eba4c10dc6e2591ddec4b
insnergy <developer@insnergy.com>
2017-05-10 17:17:07 +0800

    first commit
    hello world sample

---
+++ app/src/main/java/com/insnergy/sample/MainApplication.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2017 The InApi Project
+ */
+package com.insnergy.sample;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+    private static final String TAG = "MainApplication";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/insnergy/sample/view/MainActivity.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2017 The InApi Project
+ */
+package com.insnergy.sample.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.insnergy.sample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
